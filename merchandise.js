3eca537c562c2c36c556100cd94644af251fa9ad
Maoning Tang <maoning.tang@Maonings-MacBook-Pro.local>
2020-05-05 02:11:03 +0800

    Merchandise should update its quality and sellIn base on its depreciation rate

--- merchandise.js
+++ merchandise.js
@@ -2,15 +2,25 @@ export class Merchandise {
     name
     sellIn
     quality
+    depreciationRate
 
     constructor(name, sellIn, quality, depreciationRate) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.depreciationRate = depreciationRate;
+    }
+
+    updateQualityAndSellInByOneDay() {
+        this.sellIn--;
+        if (this.sellIn >= 0) {
+            this.quality -= this.depreciationRate;
+        } else {
+            this.quality -= 2 * this.depreciationRate;
+        }
     }
 
     toString() {
         return `Name: ${this.name}, SellIn: ${this.sellIn}, Quality: ${this.quality}`
     }
-
 }
--- merchandise.test.js
+++ merchandise.test.js
@@ -6,4 +6,24 @@ describe("MerchandiseTest", () => {
         let merchandiseString = merchandise.toString();
         expect(merchandiseString).toEqual("Name: ordinary, SellIn: 1, Quality: 3");
     });
+
+    test("it should update quality and sellIn by one day", () => {
+        const merchandise = new Merchandise('ordinary', 1, 3, 1);
+        expect(merchandise.name).toEqual('ordinary');
+        expect(merchandise.sellIn).toEqual(1);
+        expect(merchandise.quality).toEqual(3);
+        merchandise.updateQualityAndSellInByOneDay();
+        expect(merchandise.sellIn).toEqual(0);
+        expect(merchandise.quality).toEqual(2);
+    });
+
+    test("it should double the depreciationRate when merchandise expired", () => {
+        const merchandise = new Merchandise('ordinary', 0, 3, 1);
+        expect(merchandise.name).toEqual('ordinary');
+        expect(merchandise.sellIn).toEqual(0);
+        expect(merchandise.quality).toEqual(3);
+        merchandise.updateQualityAndSellInByOneDay();
+        expect(merchandise.sellIn).toEqual(-1);
+        expect(merchandise.quality).toEqual(1);
+    });
 });
